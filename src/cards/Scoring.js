0307ddda764d62cd9825aff1c3a16a414668b306
GSeal <sealgp1@student.op.ac.nz>
2023-06-12 12:37:54 +1200

    adjust scoring to use all cards in hand

--- src/cards/Scoring.js
+++ src/cards/Scoring.js
@@ -3,10 +3,10 @@ export const CalculateScore = (cardHand) => {
 
   const sortableHand = cardHand?.map(c => c)
   const sortedHand = sortableHand?.sort((a, b) => {
-    return a.value - b.value
+    return  b.value - a.value
   })
   
-  const cardCount = sortedHand?.reduce((tally, card) => {
+  const findMatchingCards = sortedHand?.reduce((tally, card) => {
     tally[card.value] = (tally[card.value] || 0) + 1;
     return tally;
   }, {});
@@ -15,80 +15,124 @@ export const CalculateScore = (cardHand) => {
     return Object.keys(obj).find(key => obj[key] === value)
   }
 
+  console.log(findMatchingCards)
+  console.log(Object?.values(findMatchingCards))
+
   for (let index = 0; index < functions.length; index++) {
     const f = functions[index];
-    const score = f(sortedHand, cardCount, getObjectKey)
+    const score = f(sortedHand, findMatchingCards, getObjectKey)
     if (score) {
       return score
     }
   }
-  return parseInt(sortedHand[4].value);
+  return addCardValues(sortedHand);
+}
+
+// https://www.tutorialspoint.com/sorting-only-a-part-of-an-array-javascript
+// if there are matching cards in they should stay at the front of the array, only remaining cards should be sorted
+const sortUnmatchingCards = (cards, start, end) => {
+  const unmatching = cards.splice(start, end - start + 1)
+  unmatching.sort(function(a, b){return b-a});
+  cards.splice(start, 0, ...unmatching)
+}
+
+const addCardValues = (cards) => {
+  let total= 0;
+  let multiplier = 10000
+  for (let i = 0; i < cards.length; i++) {
+    total = total + (cards[i] * multiplier)
+    multiplier /= 10
+  }
+  return total
+
 }
   
 const functions = [];
 
-const royalFlush = (sortedHand, cardCount, getObjectKey) => {
-  console.log(sortedHand)
+const royalFlush = (sortedHand, findMatchingCards, getObjectKey) => {
   if (
     sortedHand?.filter((card) => card.suit === sortedHand[0].suit)
       .length === 5 &&
-    sortedHand[0]?.value + 4 === sortedHand[4]?.value && 
-    sortedHand[0]?.value === 10
+    sortedHand[0]?.value - 4 === sortedHand[4]?.value && 
+    (Object?.values(findMatchingCards).length === 5) &&
+    sortedHand[0]?.value === 14
   ) {
-    return 900 + parseInt(sortedHand[4].value);
+    return 9000000 + addCardValues(sortedHand);
   }
 }
 
-const straightFlush = (sortedHand, cardCount, getObjectKey) => { 
+const straightFlush = (sortedHand, findMatchingCards, getObjectKey) => { 
   if (
     (sortedHand.filter((card) => card.suit === sortedHand[0].suit)
       .length === 5) &&
-    (sortedHand[0]?.value + 4 === sortedHand[4]?.value)
+    (sortedHand[0]?.value - 4 === sortedHand[4]?.value) &&
+    (Object?.values(findMatchingCards).length === 5)
   ) {
-    return 800 + parseInt(sortedHand[0].value);
+    return 8000000 + addCardValues(sortedHand);
   }
 }
 
-const fourOfAKind = (sortedHand, cardCount, getObjectKey) => {
-  if (Object?.values(cardCount).includes(4)) {
-    return 700 + parseInt(getObjectKey(cardCount, 4));
+const fourOfAKind = (sortedHand, findMatchingCards, getObjectKey) => {
+  if (Object?.values(findMatchingCards).includes(4)) {
+    return 7000000 + addCardValues(sortedHand);
   }
 }
 
-const fullHouse = (sortedHand, cardCount, getObjectKey) => { 
-  if ((Object?.values(cardCount).includes(2)) && (Object?.values(cardCount).includes(3))) {
+const fullHouse = (sortedHand, findMatchingCards, getObjectKey) => { 
+  if ((Object?.values(findMatchingCards).includes(2)) && (Object?.values(findMatchingCards).includes(3))) {
     console.log("here")
-    return 600 + parseInt(sortedHand[0].value)
+    return 6000000 + addCardValues(sortedHand);
   }
 }
 
-const flush = (sortedHand, cardCount, getObjectKey) => { 
+const flush = (sortedHand, findMatchingCards, getObjectKey) => { 
   if (sortedHand.filter(card => card.suit === sortedHand[0].suit).length === 5) {
-    return 500 + parseInt(sortedHand[0].value);
+    return 5000000 + addCardValues(sortedHand);
   }
 }
 
-const straight = (sortedHand, cardCount, getObjectKey) => { 
-  if ((sortedHand[0]?.value + 4) === (sortedHand[4]?.value)){
-    return 400 + parseInt(sortedHand[0].value);
+const straight = (sortedHand, findMatchingCards, getObjectKey) => { 
+  if ((sortedHand[0]?.value - 4) === (sortedHand[4]?.value) && Object?.values(findMatchingCards).length === 5){
+    return 4000000 + addCardValues(sortedHand);
   }
 }
 
-const threeOfKind = (sortedHand, cardCount, getObjectKey) => { 
-  if (Object?.values(cardCount).includes(3)) {
-    return 300 + parseInt(getObjectKey(cardCount, 3));
+// extracting card values from sorted matching cards array
+const createScorableArray = (findMatchingCards, start, end) => {
+
+  const sortedMatchingCards = (Object.entries(findMatchingCards).sort((a,b) => b[1]-a[1]))
+  const scorableHand = []
+  sortedMatchingCards.forEach(card => {
+    scorableHand.push(card[0])
+  });
+  sortUnmatchingCards(scorableHand, start, end)
+  
+  return scorableHand
+}
+
+
+
+const threeOfKind = (sortedHand, findMatchingCards, getObjectKey) => { 
+  if (Object?.values(findMatchingCards).includes(3)) {
+
+    const scorableHand = createScorableArray(findMatchingCards, 1, 2)
+
+    return 3000000 + addCardValues(scorableHand);
   }
 }
 
-const twoPairs = (sortedHand, cardCount, getObjectKey) => {
-  if (Object?.values(cardCount).includes(2) && Object?.values(cardCount).length === 3) {
-    return 200 + parseInt(getObjectKey(cardCount, 2));
+const twoPairs = (sortedHand, findMatchingCards, getObjectKey) => {
+  if (Object?.values(findMatchingCards).includes(2) && Object?.values(findMatchingCards).length === 3) {
+    return 2000000 + addCardValues(sortedHand);
   }
 }
 
-const pair = (sortedHand, cardCount, getObjectKey) => { 
-  if (Object?.values(cardCount).includes(2)) {
-    return 100 + parseInt(getObjectKey(cardCount, 2));
+const pair = (sortedHand, findMatchingCards, getObjectKey) => { 
+  if (Object?.values(findMatchingCards).includes(2)) {
+
+    const scorableHand = createScorableArray(findMatchingCards, 1, 3)
+    
+    return 1000000 + addCardValues(scorableHand);
   }
 }
 
